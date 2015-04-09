8da7c4a201147755141023c2f3a580999f0ed784
Gleb Vorobjev <glebvorobjev@gmail.com>
2015-04-09 17:54:04 -0400

    Add test that flash error message does not persist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log user in
     else
-      # Display error message
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
