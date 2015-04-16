5cbdb990c6be066b42f4664ba5d02f04a1374b35
Gleb Vorobjev <glebvorobjev@gmail.com>
2015-04-16 17:39:31 -0400

    Refactor authenticated? to check any given digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,9 +35,10 @@ class User < ActiveRecord::Base
   end
 
   # Returns true if given token matches hashed version in database
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
